cccda4e950075994511c93bc48b6dfc9eaced129
masha <mashajs2021@gmail.com>
2021-01-22 09:04:26 -0500

    add dropdown class, style this class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import Tooltip from "./ui_components/tooltip";
-//create a tooltip
+import Dropdown from "./ui_components/dropdown";
 
 const tooltip = new Tooltip(document.querySelector("span.tooltip"));
 tooltip.init();
+
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
